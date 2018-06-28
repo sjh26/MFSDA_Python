eebe7222723760290c4b2699d26a80ee90386f64
Loic Michoud <lo.michoud@gmail.com>
2018-06-28 14:15:45 -0400

    ENH: new input files to run MFSDA

--- MFSDA/MFSDA.py
+++ MFSDA/MFSDA.py
@@ -1,19 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import os
 import unittest
 import vtk, qt, ctk, slicer
 from slicer.ScriptedLoadableModule import *
 import logging
-import inputData
+#import inputData
 import sys
 import numpy as np
 from scipy import stats
+import pandas as pd
 #from scipy.io import loadmat
 from statsmodels.sandbox.stats.multicomp import fdrcorrection0
-from stat_read_x import read_x
-from stat_lpks import lpks
-from stat_sif import sif
-from stat_wald_ht import wald_ht
-from stat_bstrp_pvalue import bstrp_pvalue
+#from stat_read_x import read_x
+#from stat_lpks import lpks
+#from stat_sif import sif
+#from stat_wald_ht import wald_ht
+#from stat_bstrp_pvalue import bstrp_pvalue
 import inspect
 cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"lib")))
 if cmd_subfolder not in sys.path:
@@ -80,28 +83,21 @@ class MFSDAWidget(ScriptedLoadableModuleWidget):
         
         self.lineEdit_vtk = self.logic.get('lineEdit_vtk')
         self.lineEdit_template = self.logic.get('lineEdit_template')
-        self.lineEdit_covariate = self.logic.get('lineEdit_covariate')
-        self.lineEdit_covariateType = self.logic.get('lineEdit_covariateType')
+        self.lineEdit_csv = self.logic.get('lineEdit_csv')
         self.lineEdit_output = self.logic.get('lineEdit_output')
-        self.lineEdit_CovariateNames = self.logic.get('lineEdit_CovariateNames')
         self.lineEdit_ShapePvalue=self.logic.get('lineEdit_ShapePvalue')
-        self.lineEdit_covariateType.connect('currentPathChanged(const QString)', self.onCSVFile)
-        self.lineEdit_covariate.connect('currentPathChanged(const QString)', self.onCSVFile)
+        self.lineEdit_csv.connect('directoryChanged(const QString)', self.onCSVFile)
         self.lineEdit_template.connect('currentPathChanged(const QString)', self.onCSVFile)
         self.lineEdit_vtk.connect('currentPathChanged(const QString)', self.onCSVFile)
         self.lineEdit_output.connect('directoryChanged (const QString)', self.onCSVFile)
         self.lineEdit_ShapePvalue.connect('directoryChanged (const QString)', self.onCSVFile)
-        self.lineEdit_CovariateNames.connect('directoryChanged (const QString)', self.onCSVFile)
     
     
     
     
     def onCSVFile(self):
         self.dictShapeModels = dict()
-        if not os.path.exists(self.lineEdit_covariateType.currentPath):
-            self.stateCSVMeansShape = False
-            return
-        if not os.path.exists(self.lineEdit_covariate.currentPath):
+        if not os.path.exists(self.lineEdit_csv.currentPath):
             self.stateCSVMeansShape = False
             return
         if not os.path.exists(self.lineEdit_template.currentPath):
@@ -116,17 +112,11 @@ class MFSDAWidget(ScriptedLoadableModuleWidget):
         if not os.path.exists(self.lineEdit_ShapePvalue.currentPath):
             self.stateCSVMeansShape = False
             return
-        if not os.path.exists(self.lineEdit_CovariateNames.currentPath):
-            self.stateCSVMeansShape = False
-            return
         condition1 = self.logic.checkExtension(self.lineEdit_vtk.currentPath, ".txt")
         condition2 = self.logic.checkExtension(self.lineEdit_template.currentPath, ".vtk")
-        condition3 = self.logic.checkExtension(self.lineEdit_covariate.currentPath, ".csv")
-        condition4 = self.logic.checkExtension(self.lineEdit_covariateType.currentPath, ".csv")
+        condition4 = self.logic.checkExtension(self.lineEdit_csv.currentPath, ".csv")
         condition5= self.lineEdit_output.directory!='.'
         condition6 = self.logic.checkExtension(self.lineEdit_ShapePvalue.currentPath, ".vtk")
-        condition7= self.lineEdit_CovariateNames.currentPath!='.'
-        
         if not condition1:
             self.lineEdit_vtk.setCurrentPath(" ")
             self.stateCSVDataset = False
@@ -134,46 +124,69 @@ class MFSDAWidget(ScriptedLoadableModuleWidget):
         if not condition2:
             self.lineEdit_template.setCurrentPath(" ")
             self.stateCSVDataset = False
-        return
-        if not condition3:
-            self.lineEdit_covariate.setCurrentPath(" ")
-            self.stateCSVDataset = False
-        return
+            return
         if not condition4:
-            self.lineEdit_covariateType.setCurrentPath(" ")
+            self.lineEdit_csv.setCurrentPath(" ")
             self.stateCSVDataset = False
-        return
+            return
         if not condition5:
-            #self.lineEdit_output.setDirectory(" ")
             self.stateCSVDataset = False
-        return
+            return
         if not condition6:
             self.lineEdit_ShapePvalue.setCurrentPath(" ")
             self.stateCSVDataset = False
-        return
-        if not condition7:
-            self.lineEdit_CovariateNames.setCurrentPath(" ")
-            self.stateCSVDataset = False
-        return
+            return
         PathOutput=os.path.dirname(self.lineEdit_vtk.currentPath)+'/'
-        
-        print(self.lineEdit_covariateType.currentPath)
-        print(self.lineEdit_covariate.currentPath)
-        print(self.lineEdit_template.currentPath)
-        print(self.lineEdit_vtk.currentPath)
-        print(self.lineEdit_output.directory)
-        print(self.lineEdit_ShapePvalue.currentPath)
-        print(self.lineEdit_CovariateNames.currentPath)
-        print(PathOutput)
-        
-        
-        args=arguments(coordData=self.lineEdit_template.currentPath, covariate=self.lineEdit_covariate.currentPath, covariateType=self.lineEdit_covariateType.currentPath, outputDir=self.lineEdit_output.directory, shapeData=self.lineEdit_vtk.currentPath, shapePath=PathOutput)
-        run_script(args)
+
+
+        covariates_sa = pd.read_csv(self.lineEdit_csv.currentPath)
+        covariates = np.array(covariates_sa.axes[1])
+        if type(covariates[0]) == str:
+            covariates_sa = pd.read_csv(self.lineEdit_csv.currentPath, usecols=covariates)
+        covariates_sa = np.array(covariates_sa)
+        #######################################################################
+        # Reconstruct cvs file without the name of the covariates
+        CSVFile=open(self.lineEdit_output.directory+'/valuesCsv.csv', 'wb')
+        with CSVFile:
+            writer = csv.writer(CSVFile)
+            for i in range(0,len(covariates_sa)):
+                writer.writerow(covariates_sa[i])
+        CSVFile.close()
+        #######################################################################
+        # Create the covariateType file (1 or 0 to indicate type of covariate double or int)
+        covariateType = np.ones((1,len(covariates)))
+        for j in range(0,len(covariates)-1):
+            if isinstance(covariates_sa[0][j], int):
+                covariateType[j] = 0
+        if os.path.exists(self.lineEdit_output.directory+'/CovariateType.csv'):
+            os.remove(self.lineEdit_output.directory+'/CovariateType.csv')
+        CSVFile=open(self.lineEdit_output.directory+'/CovariateType.csv', 'wb')
+        with CSVFile:
+            writer = csv.writer(CSVFile)
+            writer.writerow(covariateType[0])
+        CSVFile.close()
+        ######################################################################
+        # Rewrite txt file with full path to the vtk files
+        tempPath = self.lineEdit_output.directory+'/tempText.txt'
+        if os.path.exists(tempPath):
+            os.remove(tempPath)
+        TXTFile=open(tempPath, 'a')
+        fh = open(self.lineEdit_vtk.currentPath, 'rU')
+        for vtkfilename in fh.readlines():
+            vtkfilename = os.path.dirname(self.lineEdit_vtk.currentPath) + '/' + vtkfilename.rstrip()
+            TXTFile.write(vtkfilename)
+            TXTFile.write("\n")
+        fh.close()        
+        TXTFile.close()   
+        args=arguments(coordData=self.lineEdit_template.currentPath, covariate=self.lineEdit_output.directory+'/valuesCsv.csv', covariateType=self.lineEdit_output.directory+'/CovariateNames.csv', outputDir=self.lineEdit_output.directory, shapeData=tempPath, shapePath=PathOutput)
+        # run_script(args)
         pvaluesPath=self.lineEdit_output.directory+'/pvalues.json'
         efitPath=self.lineEdit_output.directory+'/efit.json'
         ShapePvalue=self.lineEdit_ShapePvalue.currentPath
         outputPath=self.lineEdit_output.directory+'/out.vtk'
-        argShapes=arguments(pvalues=pvaluesPath, covariates=self.lineEdit_CovariateNames.currentPath, shape=self.lineEdit_ShapePvalue.currentPath, efit=efitPath, output=outputPath)
+        covariates = covariates.tolist()
+        print("covariates dans MFSDA", covariates)
+        argShapes=arguments(pvalues=pvaluesPath, covariates=covariates, shape=ShapePvalue, efit=efitPath, output=outputPath)
         run_script2(argShapes)
         CSVFile=open(self.lineEdit_output.directory+'/output.csv', 'wb')
         Data=['VTK Files']
@@ -182,12 +195,10 @@ class MFSDAWidget(ScriptedLoadableModuleWidget):
             writer.writerow(Data)
             writer.writerow([outputPath])
         CSVFile.close()
-#        parameters = {}
-#        parameters["CSVFile"] = self.lineEdit_output.directory+'/output.csv'
-#        module = slicer.modules.shapepopulationviewer
-#        slicer.cli.run(module, None, parameters, wait_for_completion=True)
-
-
+        parameters = {}
+        parameters["CSVFile"] = self.lineEdit_output.directory+'/output.csv'
+        module = slicer.modules.shapepopulationviewer
+        slicer.cli.run(module, None, parameters, wait_for_completion=True)
 
 #
 # MFSDALogic
@@ -207,8 +218,8 @@ class MFSDALogic(ScriptedLoadableModuleLogic):
         self.interface = interface
         self.table = vtk.vtkTable
         self.colorBar = {'Point1': [0, 0, 1, 0], 'Point2': [0.5, 1, 1, 0], 'Point3': [1, 1, 0, 0]}
-        self.input_Data = inputData.inputData()
-    
+#        self.input_Data = inputData.inputData()
+
     
     def get(self, objectName):
         
@@ -261,43 +272,50 @@ class arguments:
 class MFSDATest(ScriptedLoadableModuleTest):
     
     
-    def runTest(self):
-        cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"Testing")))
-        print("in the Test")
-        vtk_currentPath=cmd_subfolder+'/shapeData.txt'
-        template_currentPath=cmd_subfolder+'/ALLM_aligned.vtk'
-        output_directory=cmd_subfolder+'/output'
-        try:
-            os.stat(output_directory)
-        except:
-            os.mkdir(output_directory)
-        covariate_currentPath=cmd_subfolder+'/TEST/covariate.csv'
-        covariateType_currentPath=cmd_subfolder+'/TEST/covariateType.csv'
-        ShapePvalue_currentPath=cmd_subfolder+'/01_Left_aligned.vtk'
-        CovariateNames_currentPath=cmd_subfolder+'/TEST/covariate'
-        PathOutput=os.path.dirname(vtk_currentPath)+'/'
-        fshape = open(vtk_currentPath, 'rU')
-        copy=PathOutput+'/temp.txt'
-        ftemp= open(copy,'w')
-        for vtkfilename in fshape.readlines():
-            ftemp.write(PathOutput+vtkfilename)
-        fshape.close()
-        ftemp.close()
-        args=arguments(coordData=template_currentPath, covariate=covariate_currentPath, covariateType=covariateType_currentPath, outputDir=output_directory, shapeData=copy, shapePath=PathOutput)
-        run_script(args)
-        pvaluesPath=output_directory+'/pvalues.json'
-        efitPath=output_directory+'/efit.json'
-        ShapePvalue=ShapePvalue_currentPath
-        outputPath=output_directory+'/out.vtk'
-        argShapes=arguments(pvalues=pvaluesPath, covariates=CovariateNames_currentPath, shape=ShapePvalue_currentPath, efit=efitPath, output=outputPath)
-        run_script2(argShapes)
-        CSVFile=open(output_directory+'/output.csv', 'wb')
-        Data=['VTK Files']
-        with CSVFile:
-            writer = csv.writer(CSVFile)
-            writer.writerow(Data)
-            writer.writerow([outputPath])
-        CSVFile.close()
+    # def runTest(self):
+    #     cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"Testing")))
+    #     print("in the Test")
+    #     vtk_currentPath=cmd_subfolder+'/shapeData.txt'
+    #     template_currentPath=cmd_subfolder+'/ALLM_aligned.vtk'
+    #     output_directory=cmd_subfolder+'/output'
+    #     try:
+    #         os.stat(output_directory)
+    #     except:
+    #         os.mkdir(output_directory)
+    #     covariate_currentPath=cmd_subfolder+'/TEST/covariate.csv'
+    #     covariateType_currentPath=cmd_subfolder+'/TEST/covariateType.csv'
+    #     ShapePvalue_currentPath=cmd_subfolder+'/01_Left_aligned.vtk'
+    #     CovariateNames_currentPath=cmd_subfolder+'/TEST/covariate'
+    #     PathOutput=os.path.dirname(vtk_currentPath)+'/'
+    #     fshape = open(vtk_currentPath, 'rU')
+    #     copy=PathOutput+'/temp.txt'
+    #     ftemp= open(copy,'w')
+    #     for vtkfilename in fshape.readlines():
+    #         ftemp.write(PathOutput+vtkfilename)
+    #     fshape.close()
+    #     ftemp.close()
+    #     args=arguments(coordData=template_currentPath, covariate=covariate_currentPath, covariateType=covariateType_currentPath, outputDir=output_directory, shapeData=copy, shapePath=PathOutput)
+    #     run_script(args)
+    #     pvaluesPath=output_directory+'/pvalues.json'
+    #     efitPath=output_directory+'/efit.json'
+    #     ShapePvalue=ShapePvalue_currentPath
+    #     outputPath=output_directory+'/out.vtk'
+    #     argShapes=arguments(pvalues=pvaluesPath, covariates=CovariateNames_currentPath, shape=ShapePvalue_currentPath, efit=efitPath, output=outputPath)
+    #     run_script2(argShapes)
+    #     CSVFile=open(output_directory+'/output.csv', 'wb')
+    #     Data=['VTK Files']
+    #     with CSVFile:
+    #         writer = csv.writer(CSVFile)
+    #         writer.writerow(Data)
+    #         writer.writerow([outputPath])
+    #     CSVFile.close()
+
+
+
+
+###################################################################################################
+###################################################################################################
+        ###########################################################################################
     #        parameters = {}
     #        parameters["CSVFile"] = output_directory+'/output.csv'
     #        module = slicer.modules.shapepopulationviewer
